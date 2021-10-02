ca01789155cc2454109480da7e42f7baaf43426b
Sahak-S <sahak.sa@mail.ru>
2021-10-02 13:40:24 +0400

    homework

---
+++ src/Chapter1/BlokTest.java
@@ -0,0 +1,14 @@
+package Chapter1;
+
+public class BlokTest {
+    public static void main(String[] args) {
+        int x, y;
+        y = 20;
+        for (x = 0; x < 10; x = x + 1){
+            System.out.print(" x =" + x);
+            y = y - 2;
+            System.out.println(" y =" + y);
+
+        }
+    }
+}
---
+++ src/Chapter1/IfSample.java
@@ -0,0 +1,17 @@
+package Chapter1;
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x, y;
+        x = 10;
+        y = 20;
+        if (x<y) System.out.println("x<y");
+        x = x * 2;
+        if (x == y) System.out.println("x = y");
+        x = x * 2;
+        if (x > y) System.out.println("x > y");
+
+    }
+
+
+}
