92f627ea265b7ffc087319655ce01b4366f3af7d
Sahak-S <sahak.sa@mail.ru>
2021-10-04 10:32:01 +0400

    ավելացրել եմ՝ BootTest,CharDemo,CharDemo2,ClasArea,ClasLight

---
+++ src/Lesson3/ClassLight.java
@@ -0,0 +1,26 @@
+package Lesson3;
+// որոշել  լույսի արագուտյունը
+// օգտագործելով  long օպերատորը
+
+public class ClassLight {
+    public static void main(String[] args) {
+        int lightspeed;
+        long deys;
+        long seconds;
+        long distance;
+        // մոտավոր լույսի արագուտյունը, կմ*վ
+
+        lightspeed = 186000;
+
+        // օրերի քանակը
+        deys = 1000;
+        //դարցնել վրկ․
+        seconds = deys * 24 * 60 * 60;
+        //հաշվել արագությունը
+        distance = lightspeed * seconds;
+        System.out.print(deys);
+        System.out.print(" օր հետո լույսը կհասնի ");
+        System.out.println(distance + " կմ ");
+
+    }
+}
