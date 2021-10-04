a99aa1bcb05d77bed3e66251af2a8513be1f2139
Sahak-S <sahak.sa@mail.ru>
2021-10-04 15:25:41 +0400

    add Dynlnit

---
+++ src/Lesson3/DynInit.java
@@ -0,0 +1,9 @@
+package Lesson3;
+
+public class DynInit {
+    public static void main(String[] args) {
+        double a =3, b = 4;
+        double c = Math.sqrt(a * a + b * b);
+        System.out.println("վարկածն հավասար է" + c);
+    }
+}
