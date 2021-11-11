39ab6b67758a6f71f0b3b81b30f68deaac7168bc
Sahak-S <sahak.sa@mail.ru>
2021-11-12 02:36:50 +0400

    homework BraceCheker,BraceChekerTest,Stace

---
+++ src/Homework/bracechecker/BraceChecker.java
@@ -0,0 +1,67 @@
+package Homework.bracechecker;
+
+public class BraceChecker {
+
+
+    //սա հենց տեքստն է
+    private String text;
+
+    //կլասսի կոնստրուկտորն է, որը պետք է ընդունի String տիպի տեքստ, և վերագրի վերևի տեքստին
+    //Որ չկարողանանք BraceChecker-ի օբյեկտը սարքենք առանց text-տալու կոնստրուկտորի միջոցով․
+    public BraceChecker(String text) {
+        this.text = text;
+    }
+
+    //սա հիմնական մեթոդն է, որի մեջ գրելու ենք ամբողջ լոգիկան․ աշխատելու ենք թե stack-ի հետ, թե վերևի text-ի
+    public void check() {
+        Stack myStack = new Stack();
+        char[] chars = text.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '(' || chars[i] == '[' || chars[i] == '{') {
+                myStack.push(chars[i]);
+            }
+
+        }
+        char c;
+        for (int i = chars.length - 1; i > 0; i--) {
+            switch (chars[i]) {
+                case ')':
+                    c = (char) myStack.pop();
+                    if (c == '{') {
+                        System.err.println(" Error ( սխալ է ) : open " + c + " and closed" + chars[i] + " at " + (i + 1));
+                    } else if (c == '[') {
+                        System.err.println(" Error ( սխալ է ) : open " + c + " and closed" + chars[i] + " at " + (i + 1));
+                    } else if (c == '(') {
+                        System.out.println(" Right ( ճիշտ է ) : open " + c + " and closed" + chars[i] + " at " + (i + 1));
+                    }
+
+                    break;
+                case '}':
+                    c = (char) myStack.pop();
+                    if (c == '(') {
+                        System.err.println(" Error ( սխալ է ) : open " + c + " and closed " + chars[i] + " at " + (i + 1));
+                    } else if (c == '[') {
+                        System.err.println(" Error ( սխալ է ) : open " + c + " and closed " + chars[i] + " at " + (i + 1));
+                    } else if (c == '{') {
+                        System.out.println(" Right ( ճիշտ է ) : open " + c + " and closed" + chars[i] + " at " + (i + 1));
+                    }
+                    break;
+                case ']':
+                    c = (char) myStack.pop();
+                    if (c == '(') {
+                        System.err.println(" Error ( սխալ է ) : open " + c + " and closed " + chars[i] + " at " + (i + 1));
+                    } else if (c == '{') {
+                        System.err.println(" Error ( սխալ է ) : open " + c + " and closed " + chars[i] + " at " + (i + 1));
+                    } else if (c == '[') {
+                        System.out.println(" Right ( ճիշտ է ) : open " + c + " and closed" + chars[i] + " at " + (i + 1));
+                    }
+                    break;
+                default:
+
+            }
+        }
+
+
+    }
+}
---
+++ src/Homework/bracechecker/BraceCheckerTest.java
@@ -0,0 +1,13 @@
+package Homework.bracechecker;
+
+public class BraceCheckerTest {
+
+    public static void main(String[] args) {
+        String text = "[Hello] (from} {Java). [Javan] {is) a {good] (program).";
+
+        BraceChecker braceChecker = new BraceChecker(text);
+        braceChecker.check();
+
+
+    }
+}
